8579270cb2c2dfb584a6131926154b1a9c16406c
Michael Zatlin <michael.zatlin@yahoo.com>
2019-10-08 22:59:02 -0700

    Implemented Basic Billboard Effect

---
+++ Cryptid Game Jam/Assets/BillboardEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardEffect : MonoBehaviour
+{
+
+    Camera mainCam;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mainCam = Camera.main;      
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.forward = -mainCam.transform.forward;
+    }
+}
