595fff1b2e383c41ebf13851cbb4bbe9c53ee991
Michael Zatlin <michael.zatlin@yahoo.com>
2019-10-23 00:13:49 -0700

    Created "current quest" ui and writer class and reorganized class files

---
+++ Cryptid Game Jam/Assets/Scripts/HeaderText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu]
+public class HeaderText : ScriptableObject
+{
+    public event Action OnWrite = delegate { };
+    public string message;
+    
+    public void WriteHeader(string _header)
+    {
+        message = _header;
+        OnWrite();
+    }
+
+}
---
+++ Cryptid Game Jam/Assets/Scripts/HeaderUpdateController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HeaderUpdateController : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI textContent;
+    [SerializeField]
+    HeaderText header;
+    [SerializeField]
+    private float typingSpeed = 0.05f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        header.message = "";
+        textContent.text = "";
+        header.OnWrite += HandleWrite;
+    }
+
+    void Start()
+    {
+        header.WriteHeader("Head to Boat");
+    }
+
+    void HandleWrite()
+    {
+        StartCoroutine(TypeMessage(header.message));
+    }
+
+    IEnumerator TypeMessage(string content)
+    {
+        foreach(char i in content.ToCharArray())
+        {
+            textContent.text += i;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+      
+    }
+
+}
