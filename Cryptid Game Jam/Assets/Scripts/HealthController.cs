f7287b916054acf58a2eeb987209f9bb22cf1a0b
Michael <michaelkhanusiak@gmail.com>
2019-10-29 12:41:29 -0400

    Damage sound test
    damage test sounds on old man and nearby kelpie in the sample scene

--- Cryptid Game Jam/Assets/Scripts/HealthController.cs
+++ Cryptid Game Jam/Assets/Scripts/HealthController.cs
@@ -5,6 +5,10 @@ using System;
 
 public class HealthController : HealthBase
 {
+    [FMODUnity.EventRef]
+    public string path;
+    FMOD.Studio.EventInstance hit;
+
     public event Action OnDie = delegate { };
     [SerializeField]
     private int currentHealth = 1;
@@ -16,10 +20,15 @@ public class HealthController : HealthBase
     public int CurrentHealth { get => currentHealth; set => currentHealth = value; }
     public int MaxHealth { get => maxHealth; set => maxHealth = value; }
     public bool IsDead { get => isDead; set => isDead = value; }
-
+    private void Start()
+    {
+        hit = FMODUnity.RuntimeManager.CreateInstance(path);
+    }
     protected override void ProcessDamage()
     {
         if (IsDead) return;
+        hit.start();
+        hit.release();
         base.ProcessDamage();
         CurrentHealth -= 1;
         if(currentHealth <= 0)
