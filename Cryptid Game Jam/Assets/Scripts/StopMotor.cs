25857f9782e6158ffcd0fd6fb3b85e10ee9e1321
Michael <michaelkhanusiak@gmail.com>
2019-10-29 08:05:24 -0400

    StopMotor back to original

--- Cryptid Game Jam/Assets/Scripts/StopMotor.cs
+++ Cryptid Game Jam/Assets/Scripts/StopMotor.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 public class StopMotor : MonoBehaviour
 {
     public event Action OnStopMotor = delegate { };
-    [FMODUnity.EventRef]
-    public string path;
 
     [SerializeField]
     PlayerStatsSO playerStats;
@@ -16,7 +14,6 @@ public class StopMotor : MonoBehaviour
         if (playerStats.isOccupied && Input.GetKeyDown(KeyCode.Escape) && BoatMovementController.isMotorActive)
         {
             OnStopMotor();
-            FMODUnity.RuntimeManager.PlayOneShot(path);
         }
     }
 }
