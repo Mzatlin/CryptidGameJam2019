a228edd32f883ddf1c88a1d242d7bb3fda969c70
Michael Zatlin <michael.zatlin@yahoo.com>
2019-10-31 01:24:32 -0700

    Added the House and Player Death

---
+++ Cryptid Game Jam/Assets/PlayerDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    HealthController health;
+    PlayerStatsSO stats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GetComponent<HealthController>();
+        health.OnDie += HandleDie;
+    }
+
+    void HandleDie() {
+        stats.isOccupied = true;
+    }
+}
