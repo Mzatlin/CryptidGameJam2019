a574dfcd846e86d44c3ca67c68ad4b00d1e91743
Mzatlin <michael.zatlin@yahoo.com>
2020-01-21 00:03:07 -0800

    Implemented Basic Fade-in Effect.

---
+++ Cryptid Game Jam/Assets/FadeInController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInController : MonoBehaviour
+{
+    [SerializeField]
+    Canvas fadeCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(fadeCanvas != null)
+        {
+            fadeCanvas.GetComponentInChildren<Image>().CrossFadeAlpha(0.1f, 2.5f, false);
+        }
+        else
+        {
+            Debug.Log("No Fade Canvas Assigned!");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
