ad2cefc637765b3d9f701a92cf9ebe15040094c1
Mzatlin <michael.zatlin@yahoo.com>
2020-10-13 20:14:11 -0700

    Enemies no longer spawn when player dies. Tweaked some assets to prevent null references/positioning.

---
+++ Cryptid Game Jam/CryptidGameJam/Assets/Scripts/SetInactiveOnDeath.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetInactiveOnDeath : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> objectsToDeactivate = new List<GameObject>();
+    HealthController health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(objectsToDeactivate.Count < 1)
+        {
+            Debug.Log("Empty List");
+        }
+        else
+        {
+            health = GetComponent<HealthController>();
+            if(health != null)
+            {
+                health.OnDie += HandleDie;
+            }
+        }
+
+
+    }
+
+    void OnDestroy()
+    {
+        if(health != null)
+        {
+            health.OnDie -= HandleDie;
+        }
+    }
+
+    void HandleDie()
+    {
+        foreach(GameObject obj in objectsToDeactivate)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
