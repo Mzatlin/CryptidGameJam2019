38271d2a70eece2eb690aca84017a5435f5d8e17
Mzatlin <michael.zatlin@yahoo.com>
2020-10-19 18:30:27 -0700

    Created an arrow that points to an object. Reorganized files into their own seperate categories.

---
+++ Cryptid Game Jam/CryptidGameJam/Assets/Scripts/Misc/RotateToPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToPoint : MonoBehaviour
+{
+    public Transform objectLocation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(objectLocation != null)
+        {
+            transform.LookAt(objectLocation);
+        }
+
+    }
+}
