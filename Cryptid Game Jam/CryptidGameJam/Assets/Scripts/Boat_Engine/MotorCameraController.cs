0f456554a3863624f824a1c17ee834be9b1aaa39
Mzatlin <michael.zatlin@yahoo.com>
2020-10-22 19:42:39 -0700

    Kelpie now turns to a specific camera that changes depending on if the motor is currently active.

---
+++ Cryptid Game Jam/CryptidGameJam/Assets/Scripts/Boat_Engine/MotorCameraController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorCameraController : MonoBehaviour
+{
+    StopMotor stop;
+    StartMotor start;
+    Camera cam;
+
+    public static Camera boatCamera;
+
+    [SerializeField]
+    Camera changeCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        boatCamera = cam;
+        start = GetComponent<StartMotor>();
+        stop = GetComponent<StopMotor>();
+        if(start != null && stop != null)
+        {
+            start.OnStartMotor += HandleStart;
+            stop.OnStopMotor += HandleStop;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (start != null && stop != null)
+        {
+            start.OnStartMotor -= HandleStart;
+            stop.OnStopMotor -= HandleStop;
+        }
+    }
+
+    private void HandleStop()
+    {
+        boatCamera = cam;
+    }
+
+    private void HandleStart()
+    {
+        boatCamera = changeCam;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Cryptid Game Jam/CryptidGameJam/Assets/Scripts/Effects/BoatBillboardEffect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatBillboardEffect : MonoBehaviour
+{
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.forward = -MotorCameraController.boatCamera.transform.forward;
+    }
+}
